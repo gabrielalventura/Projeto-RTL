39c8c56e13a03adf07cb220b6b8a2abe188e5f76
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-10-19 14:59:39 -0300

    Criação do helper renderWithRouter e escrita dos primeiros testes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente App.js', () => {
+  it('Testa se existe no topo o conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favoriteLink).toBeInTheDocument();
+  });
+
+  it('Testa se a aplicação é redirecionada corretamente para Home', () => {
+
+  });
+});
