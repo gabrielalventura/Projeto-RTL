8034ce6dd7decdda64f522cdae57c03564a7ca7c
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-10-20 15:57:33 -0300

    Resolução da segunda demanda do requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import FavoritePokemons from '../pages/FavoritePokemons';
 import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
 
 describe('Testa o componente FavoritePokemons', () => {
   it('Testa se exibe a mensagem de `No favorite pokemon found` em caso de não haver pokemon favoritado', () => {
@@ -10,5 +11,12 @@ describe('Testa o componente FavoritePokemons', () => {
     renderWithRouter(<FavoritePokemons pokemons={ withoutFavorite } />);
     const noFavoriteText = screen.getByText('No favorite pokemon found');
     expect(noFavoriteText).toBeInTheDocument();
+  });
+
+  it('Teste se são exibidos todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+    const favorites = screen.getAllByTestId('pokemon-name');
+    expect(favorites).toHaveLength(pokemons.length);
   });
 });
