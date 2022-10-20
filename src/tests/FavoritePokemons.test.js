431175c0c10ab036725a9baf9cbc27b40c150088
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-10-20 15:10:04 -0300

    Resolução da primeira demanda do requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente FavoritePokemons', () => {
+  it('Testa se exibe a mensagem de `No favorite pokemon found` em caso de não haver pokemon favoritado', () => {
+    const withoutFavorite = [];
+
+    renderWithRouter(<FavoritePokemons pokemons={ withoutFavorite } />);
+    const noFavoriteText = screen.getByText('No favorite pokemon found');
+    expect(noFavoriteText).toBeInTheDocument();
+  });
+});
